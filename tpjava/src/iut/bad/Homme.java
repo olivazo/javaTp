6a32c5ae42ed1f4bdb03d89caf6a4e50e64d190f
oliva <ratsimbazafyzooliva@gmail.com>
2024-04-01 18:20:23 +0200

    modifier humain femme et homme

--- tpjava/src/iut/bad/Homme.java
+++ tpjava/src/iut/bad/Homme.java
@@ -4,5 +4,11 @@ public class Homme extends Humain {
 
 	// Constructeur par défaut
 	public Homme() {
+		super(); // Appel au constructeur de la classe parente Humain
 	}
+	
+	 // Constructeur avec tous les champs
+    public Homme(String nom, String prenom, int age) {
+        super(nom, prenom, age); // Appel au constructeur de la classe parente Humain
+    }
 }
